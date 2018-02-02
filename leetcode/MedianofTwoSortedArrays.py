f6402bc057423b5b25a95b64bed2b9ada8a8a948
fuziyu <fzy0728@hual.ai>
2018-02-02 15:50:41 +0800

    第四题Median of Two Sorted Arrays,算法修改

--- leetcode/MedianofTwoSortedArrays.py
+++ leetcode/MedianofTwoSortedArrays.py
@@ -6,14 +6,39 @@ class Solution(object):
         :rtype: float
         """
 
-        num = nums2+nums1
-        num = sorted(num)
+        m = len(nums1)
+        n = len(nums2)
+        if(m>n):
+            nums1,nums2,m,n = nums2,nums1,n,m
+        imin = 0
+        imax = m
+        halflen = (n+m+1)/2
+        while(imin <= imax):
+            i = (imin+imax)/2
+            j = halflen - i
+            if(i<imax and nums1[i]<nums2[j-1]):
+                imin = imin+1
+            elif(i>imin and nums2[j]<nums1[i-1]):
+                imax = imax-1
+            else:
+                maxleft = 0
+                if(i==0):
+                    maxleft = nums2[j-1]
+                elif(j==0):
+                    maxleft = nums1[i-1]
+                else:
+                    maxleft = max(nums1[i-1],nums2[j-1])
+                if((n+m)%2==1):
+                    return maxleft
+                minright = 0
+                if(i == m):
+                    minright = nums2[j]
+                elif(j == n):
+                    minright = nums1[i]
+                else:
+                    minright = min(nums1[i],nums2[j])
 
-        if(len(num)%2==0):
-            return (num[len(num)/2-1]+num[len(num)/2])/2.0
-        else:
-            return num[len(num)/2]
-
+                return (minright+maxleft)/2.0
 if __name__ == '__main__':
     s = Solution()
-    print s.findMedianSortedArrays([1,2,3],[4,5,6])
+    print s.findMedianSortedArrays([1,2,3,4],[5,6,7,8])
